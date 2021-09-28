06a8d864ba0cb52b0afc9691ee28a4075baadaaf
Young-Win <young@win.com>
2021-09-28 09:30:46 +0800

    insertion examples

--- sorting/example_test.go
+++ sorting/example_test.go
@@ -10,8 +10,10 @@ import (
 
 var (
 	selectionAlg Sorter = Selection{}
+	insertionAlg Sorter = Insertion{}
 )
 
+// ints
 func ExampleSelectionInts() {
 	ints := []int{5, 4, 5, 3, 1, 2}
 	selectionAlg.SortInts(ints)
@@ -20,6 +22,15 @@ func ExampleSelectionInts() {
 	// [1 2 3 4 5 5]
 }
 
+func ExampleInsertionInts() {
+	ints := []int{5, 4, 5, 3, 1, 2}
+	insertionAlg.SortInts(ints)
+	fmt.Println(ints)
+	// Output:
+	// [1 2 3 4 5 5]
+}
+
+// float64s
 func ExampleSelectionFloat64s() {
 	s := []float64{5.2, -1.3, 0.7, -3.8, 2.6} // unsorted
 	selectionAlg.SortFloat64s(s)
@@ -33,7 +44,21 @@ func ExampleSelectionFloat64s() {
 	// [-3.8 -1.3 0.7 2.6 5.2]
 	// [NaN -Inf 0 +Inf]
 }
+func ExampleInsertionFloat64s() {
+	s := []float64{5.2, -1.3, 0.7, -3.8, 2.6} // unsorted
+	insertionAlg.SortFloat64s(s)
+	fmt.Println(s)
 
+	s = []float64{math.Inf(1), math.NaN(), math.Inf(-1), 0.0} // unsorted
+	insertionAlg.SortFloat64s(s)
+	fmt.Println(s)
+
+	// Output: 
+	// [-3.8 -1.3 0.7 2.6 5.2]
+	// [NaN -Inf 0 +Inf]
+}
+
+// strings
 func ExampleSelectionStrings() {
 	s := []string{"S", "O", "R", "T", "E", "X", "A", "M", "P", "L", "E"} 
 	selectionAlg.SortStrings(s)
@@ -42,10 +67,27 @@ func ExampleSelectionStrings() {
 	// Output: 
 	// [A E E L M O P R S T X]
 }
+func ExampleInsertionStrings() {
+	s := []string{"S", "O", "R", "T", "E", "X", "A", "M", "P", "L", "E"} 
+	insertionAlg.SortStrings(s)
+	fmt.Println(s)
 
+	// Output: 
+	// [A E E L M O P R S T X]
+}
+
+// from file
 func ExampleSelectionFromFile() {
 	s := util.ReadAllStrings("testdata/words3.txt") 
 	selectionAlg.SortStrings(s)
+	fmt.Println(s)
+
+	// Output: 
+	// [all bad bed bug dad dim dug egg fee few for gig hut ilk jam jay jot joy men nob now owl rap sky sob tag tap tar tip wad was wee yes yet zoo]
+}
+func ExampleInsertionFromFile() {
+	s := util.ReadAllStrings("testdata/words3.txt") 
+	insertionAlg.SortStrings(s)
 	fmt.Println(s)
 
 	// Output: 
