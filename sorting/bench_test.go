5b4b805491f495789bf6b26154af2ba562d7d44c
Young-Win <young@win.com>
2021-09-29 15:11:19 +0800

    Update bench_test.go

--- sorting/bench_test.go
+++ sorting/bench_test.go
@@ -6,42 +6,39 @@ import (
 	. "github.com/youngzhu/algs4-go/sorting"
 )
 
-// go test -v -run="none" -bench="."
+// go test -v -run="none" -bench="." -benchtime="3s"
 
 var a = [...]int {9, 10, 0, 7, 8, 4, 3, 6, 2, 1, 5, 5}
 
 func BenchmarkSelection(b *testing.B) {
 	soter := Selection{}
-	s := a[0:]
 
 	b.ReportAllocs()
 	b.ResetTimer()
 
 	for i := 0; i < b.N; i++ {
-		soter.SortInts(s)
+		soter.SortInts(a[0:])
 	}
 }
 
 func BenchmarkInsertion(b *testing.B) {
 	soter := Insertion{}
-	s := a[0:]
 
 	b.ReportAllocs()
 	b.ResetTimer()
 
 	for i := 0; i < b.N; i++ {
-		soter.SortInts(s)
+		soter.SortInts(a[0:])
 	}
 }
 
 func BenchmarkShell(b *testing.B) {
 	soter := Shell{}
-	s := a[0:]
 
 	b.ReportAllocs()
 	b.ResetTimer()
 
 	for i := 0; i < b.N; i++ {
-		soter.SortInts(s)
+		soter.SortInts(a[0:])
 	}
 }
