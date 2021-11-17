5292918b54a8998f411f99030601ff281fe3fafc
Young-Win <young@win.com>
2021-11-17 11:09:25 +0800

    Update ordered_symbol_table.go

--- searching/ordered_symbol_table.go
+++ searching/ordered_symbol_table.go
@@ -1,5 +1,14 @@
 package searching
 
+// Ordered symbol tables.
+// In typical applications, keys are Comparable objects, so the option exists of
+// using a.CompareTo(b) to compare two keys a and b. Several symbol-table
+// implementations take advantage of order among the keys thai is implied by
+// Comparable to provide efficient implementations of Put() and Get() operations.
+// More important, in such implementations, we can think of the symbol table as
+// keeping the keys in order and consider a significantly expanded API that 
+// difines numerous and useful operations involving relative key order.
+
 type OrderedSymbolTable interface {
 	Put(key OrderedSTKey, value STValue)
 	Get(key OrderedSTKey) STValue
