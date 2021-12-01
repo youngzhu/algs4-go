3df05637f40edc9d06f15fd48b54af9e71eb4144
Young-Mac Pro <young.zhu.2012@gmail.com>
2021-12-01 21:28:12 +0800

    string sorts

---
+++ strings/string_sorts.go
@@ -0,0 +1,36 @@
+package strings
+
+const (
+	R      = 256 // extended ADCII alphabet size
+	cutoff = 15  // cutoff to insertion sort
+)
+
+// insertion sort a[lo..hi], starting at dth character
+func insertion(a []string, lo, hi, d int) {
+	for i := lo; i <= hi; i++ {
+		for j := i; j > lo && less(a[j], a[j-1], d); j-- {
+			a[j], a[j-1] = a[j-1], a[j]
+		}
+	}
+}
+
+// is s1 less than s2, starting at dth character
+func less(s1, s2 string, d int) bool {
+	len1, len2 := len(s1), len(s2)
+	for i := d; i < min(len1, len2); i++ {
+		if s1[i] < s2[i] {
+			return true
+		}
+		if s1[i] > s2[i] {
+			return false
+		}
+	}
+	return len1 < len2
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
