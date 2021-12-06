80a6e943e18d69810488faa0ef82a8c45004cac1
Young-Win <young@win.com>
2021-12-06 09:30:24 +0800

    Create edge.go

---
+++ graphs/edge.go
@@ -0,0 +1,36 @@
+packge graphs
+
+type Edge struct {
+	v, w int
+	weight float64
+}
+
+func NewEdge(v, w int, weight float64) Edge {
+	if v < 0 || w < 0 {
+		panic("vertex index must be a non-negative integer")
+	}
+
+	return Edge{v, w, weight}
+}
+
+/* The Either() and Other methods are usefule for accessing the edge's vertices */
+
+// Return either endpoint of this edge
+func (e Edge) Either() int {
+	return e.v
+}
+
+// Returns the endpoint of this edge that is different from the given vertex
+func (e Edge) Other(vertex int) int {
+	if vertex == e.v {
+		return v.w
+	} else if vertex == e.w {
+		return e.v
+	} else {
+		panic("Illegal endpoint")
+	}
+}
+
+func (e Edge) Weight() float64 {
+	return e.weight
+}
