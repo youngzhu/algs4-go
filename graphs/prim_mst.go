c86abe196a570d781a5120e01426063447fd4172
Young-Win <young@win.com>
2021-12-10 23:55:41 +0800

    Create prim_mst.go

---
+++ graphs/prim_mst.go
@@ -0,0 +1,9 @@
+package graphs
+
+// Proposition.
+// Prim's algorithm computes the MST of any connected edge-weighted graph. 
+// The lazy version of Prim's algorithm uses space proportional to E and time
+// proportional to ElogE (in the worst case) to compute the MST of a connected
+// edge-weighted graph with E edges and V vertices.
+// The eager version uses space proportional to V and time proportional to 
+// ElogV (in the worst case).
