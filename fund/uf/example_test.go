525ba50ee9397eed36e75b523a06fadb44b98f8c
Young-Mac Pro <young.zhu.2012@gmail.com>
2021-12-11 18:02:21 +0800

    QuickFindUF

--- fund/uf/example_test.go
+++ fund/uf/example_test.go
@@ -39,3 +39,36 @@ func ExampleUF() {
 	// 6 1
 	// 2 components
 }
+
+func ExampleQuickFindUF() {
+
+	in := util.NewInReadWords("testdata/tinyUF.txt")
+
+	n := in.ReadInt()
+	unionFind := uf.NewQuickFindUF(n)
+
+	for !in.IsEmpty() {
+		p := in.ReadInt()
+		q := in.ReadInt()
+
+		if unionFind.Find(p) == unionFind.Find(q) {
+			continue
+		}
+
+		unionFind.Union(p, q)
+		fmt.Printf("%d %d\n", p, q)
+	}
+
+	fmt.Printf("%d components", unionFind.Count())
+
+	// Output:
+	// 4 3
+	// 3 8
+	// 6 5
+	// 9 4
+	// 2 1
+	// 5 0
+	// 7 2
+	// 6 1
+	// 2 components
+}
