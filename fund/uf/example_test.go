e60d6a446dd49625488c68e72c9568c054928917
Young-Mac Pro <young.zhu.2012@gmail.com>
2021-12-12 21:00:08 +0800

    QuickUnionUF

--- fund/uf/example_test.go
+++ fund/uf/example_test.go
@@ -72,3 +72,36 @@ func ExampleQuickFindUF() {
 	// 6 1
 	// 2 components
 }
+
+func ExampleQuickUnionUF() {
+
+	in := util.NewInReadWords("testdata/tinyUF.txt")
+
+	n := in.ReadInt()
+	unionFind := uf.NewQuickUnionUF(n)
+
+	for !in.IsEmpty() {
+		p := in.ReadInt()
+		q := in.ReadInt()
+
+		if unionFind.Find(p) == unionFind.Find(q) {
+			continue
+		}
+
+		unionFind.Union(p, q)
+		fmt.Printf("%d %d\n", p, q)
+	}
+
+	fmt.Printf("%d components", unionFind.Count())
+
+	// Output:
+	// 4 3
+	// 3 8
+	// 6 5
+	// 9 4
+	// 2 1
+	// 5 0
+	// 7 2
+	// 6 1
+	// 2 components
+}
